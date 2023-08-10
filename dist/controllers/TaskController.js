51f5d4fb6c871568b86b713c2d0ada64bd7616cc
Rami-ElSkakni <sakakinirami2@gmail.com>
2023-08-10 21:31:48 +0300

    deleted log in backend

--- dist/controllers/TaskController.js
+++ dist/controllers/TaskController.js
@@ -72,7 +72,6 @@ const editTask = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { id } = req.params;
         const task = yield TaskModel_1.default.findByIdAndUpdate(id, req.body);
-        console.log(task);
         res.status(200).json({
             status: "success",
             data: task,
